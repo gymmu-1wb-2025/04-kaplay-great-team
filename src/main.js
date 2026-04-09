0ffd86d83ddf83823041efa02ea977a3a5eeb254
Sara Camenzind <e273618@sbl.ch>
2026-04-09 13:58:59 +0200

    anpassung der grösse des spieles

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import sc00 from "./scenes/sc-00";
 import sc01 from "./scenes/sc-01";
 
 const k = kaplay({
-	height: 480,
-	width: 640,
+	height: 750,
+	width: 1350,
 	canvas: document.getElementById("game-canvas"),
 	background: "#82b4b4",
 	global: false,
@@ -16,5 +16,8 @@ k.scene("init", sc00);
 k.scene("lvl-01", sc01);
 
 k.go("init");
+
+k.add([k.circle(20), k.pos(320, 240), k.color("red")]);
+k.add([k.rect(640, 20), k.pos(0, 460), k.color("green")]);
 
 export default k;
