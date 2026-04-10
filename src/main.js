75034a272dc941a82399b637b54f9af2268a838b
Sara Camenzind <e273618@sbl.ch>
2026-04-10 13:58:47 +0200

    level 2 hinzugefügt mit den goldigen äpfel

--- src/main.js
+++ src/main.js
@@ -10,105 +10,257 @@ const k = kaplay({
 	debugKey: "r",
 });
 
-// Äpfel langsamer fallen lassen
-k.setGravity(350);
+// ----------------------------
+// LEVEL 1
+// ----------------------------
+k.scene("level1", () => {
+	k.setGravity(350);
 
-// Score
-let score = 0;
+	let score = 0;
 
-const scoreText = k.add([
-	k.text("Score: 0", { size: 32 }),
-	k.pos(20, 20),
-	k.color(255, 255, 255),
-]);
-
-// Boden
-k.add([
-	k.rect(1350, 80),
-	k.pos(0, 670),
-	k.color(34, 139, 34),
-	k.area(),
-	k.body({ isStatic: true }),
-	"ground",
-]);
-
-// KORB
-// Wir machen den Korb aus einem Hauptobjekt mit Trefferfläche
-const basket = k.add([
-	k.rect(100, 40),
-	k.pos(620, 590),
-	k.color(139, 69, 19),
-	k.area(),
-	k.body({ isStatic: true }),
-	"basket",
-]);
-
-// Damit es mehr wie ein Korb aussieht
-basket.add([k.rect(100, 10), k.pos(0, 30), k.color(101, 67, 33)]);
-
-basket.add([k.rect(10, 40), k.pos(0, 0), k.color(101, 67, 33)]);
-
-basket.add([k.rect(10, 40), k.pos(90, 0), k.color(101, 67, 33)]);
-
-// Äpfel spawnen
-k.loop(2, () => {
-	const isRed = k.rand(0, 1) > 0.5;
-	const appleColor = isRed ? [255, 0, 0] : [0, 200, 0];
-	const points = isRed ? 1 : -1;
-
-	const apple = k.add([
-		k.circle(15),
-		k.pos(k.rand(50, 1300), -20),
-		k.color(appleColor[0], appleColor[1], appleColor[2]),
-		k.area(),
-		k.body(),
-		{ points: points },
-		"apple",
+	const scoreText = k.add([
+		k.text("Score: 0", { size: 32 }),
+		k.pos(20, 20),
+		k.color(255, 255, 255),
 	]);
 
-	// Wenn Apfel den Korb trifft
-	apple.onCollide("basket", () => {
-		score += apple.points;
-		scoreText.text = "Score: " + score;
-		apple.destroy();
-	});
+	// Boden
+	k.add([
+		k.rect(1350, 80),
+		k.pos(0, 670),
+		k.color(34, 139, 34),
+		k.area(),
+		k.body({ isStatic: true }),
+		"ground",
+	]);
 
-	// Wenn Apfel den Boden trifft
-	apple.onCollide("ground", () => {
-		apple.destroy();
-	});
+	// Korb
+	const basket = k.add([
+		k.rect(100, 40),
+		k.pos(620, 590),
+		k.color(139, 69, 19),
+		k.area(),
+		k.body({ isStatic: true }),
+		"basket",
+	]);
 
-	// Falls ein Apfel ganz rausfällt
-	apple.onUpdate(() => {
-		if (apple.pos.y > 750) {
+	// Korb schöner machen
+	basket.add([k.rect(100, 10), k.pos(0, 30), k.color(101, 67, 33)]);
+
+	basket.add([k.rect(10, 40), k.pos(0, 0), k.color(101, 67, 33)]);
+
+	basket.add([k.rect(10, 40), k.pos(90, 0), k.color(101, 67, 33)]);
+
+	// Äpfel in Level 1
+	k.loop(1.2, () => {
+		const isRed = k.rand(0, 1) > 0.5;
+		const appleColor = isRed ? [255, 0, 0] : [0, 200, 0];
+		const points = isRed ? 1 : -1;
+
+		const apple = k.add([
+			k.circle(15),
+			k.pos(k.rand(50, 1300), -20),
+			k.color(appleColor[0], appleColor[1], appleColor[2]),
+			k.area(),
+			k.body(),
+			{ points: points },
+			"apple",
+		]);
+
+		// Wenn Apfel den Korb trifft
+		apple.onCollide("basket", () => {
+			score += apple.points;
+			scoreText.text = "Score: " + score;
 			apple.destroy();
+
+			// Ab 10 Punkten zu Level 2
+			if (score >= 10) {
+				k.go("nextLevel", { score: score });
+			}
+		});
+
+		// Wenn Apfel den Boden trifft
+		apple.onCollide("ground", () => {
+			apple.destroy();
+		});
+
+		apple.onUpdate(() => {
+			if (apple.pos.y > 750) {
+				apple.destroy();
+			}
+		});
+	});
+
+	// Schnellere Steuerung
+	k.onKeyDown("d", () => {
+		if (basket.pos.x < 1250) {
+			basket.pos.x += 12;
+		}
+	});
+
+	k.onKeyDown("a", () => {
+		if (basket.pos.x > 0) {
+			basket.pos.x -= 12;
+		}
+	});
+
+	k.onKeyDown("right", () => {
+		if (basket.pos.x < 1250) {
+			basket.pos.x += 12;
+		}
+	});
+
+	k.onKeyDown("left", () => {
+		if (basket.pos.x > 0) {
+			basket.pos.x -= 12;
 		}
 	});
 });
 
-// SCHNELLERE STEUERUNG
-k.onKeyDown("d", () => {
-	if (basket.pos.x < 1250) {
-		basket.pos.x += 10;
-	}
+// ----------------------------
+// ZWISCHENBILDSCHIRM
+// ----------------------------
+k.scene("nextLevel", (data) => {
+	k.add([
+		k.text("Nächstes Level!\nDrücke Leertaste", {
+			size: 40,
+			align: "center",
+		}),
+		k.pos(390, 300),
+		k.color(255, 255, 255),
+	]);
+
+	k.add([
+		k.text("Dein Score: " + data.score, { size: 28 }),
+		k.pos(560, 420),
+		k.color(255, 255, 0),
+	]);
+
+	k.onKeyPress("space", () => {
+		k.go("level2", { score: data.score });
+	});
 });
 
-k.onKeyDown("a", () => {
-	if (basket.pos.x > 0) {
-		basket.pos.x -= 10;
-	}
+// ----------------------------
+// LEVEL 2
+// ----------------------------
+k.scene("level2", (data) => {
+	k.setGravity(350);
+
+	let score = data.score;
+
+	const scoreText = k.add([
+		k.text("Score: " + score, { size: 32 }),
+		k.pos(20, 20),
+		k.color(255, 255, 255),
+	]);
+
+	const levelText = k.add([
+		k.text("Level 2", { size: 28 }),
+		k.pos(20, 60),
+		k.color(255, 255, 0),
+	]);
+
+	// Boden
+	k.add([
+		k.rect(1350, 80),
+		k.pos(0, 670),
+		k.color(34, 139, 34),
+		k.area(),
+		k.body({ isStatic: true }),
+		"ground",
+	]);
+
+	// Korb
+	const basket = k.add([
+		k.rect(100, 40),
+		k.pos(620, 590),
+		k.color(139, 69, 19),
+		k.area(),
+		k.body({ isStatic: true }),
+		"basket",
+	]);
+
+	// Korb schöner machen
+	basket.add([k.rect(100, 10), k.pos(0, 30), k.color(101, 67, 33)]);
+
+	basket.add([k.rect(10, 40), k.pos(0, 0), k.color(101, 67, 33)]);
+
+	basket.add([k.rect(10, 40), k.pos(90, 0), k.color(101, 67, 33)]);
+
+	// Mehr Äpfel in Level 2
+	k.loop(0.9, () => {
+		const randomNumber = k.rand(0, 1);
+
+		let appleColor = [255, 0, 0];
+		let points = 1;
+
+		// Goldene Äpfel selten
+		if (randomNumber < 0.15) {
+			appleColor = [255, 215, 0]; // Gold
+			points = 3;
+		} else if (randomNumber < 0.55) {
+			appleColor = [255, 0, 0]; // Rot
+			points = 1;
+		} else {
+			appleColor = [0, 200, 0]; // Grün
+			points = -1;
+		}
+
+		const apple = k.add([
+			k.circle(15),
+			k.pos(k.rand(50, 1300), -20),
+			k.color(appleColor[0], appleColor[1], appleColor[2]),
+			k.area(),
+			k.body(),
+			{ points: points },
+			"apple",
+		]);
+
+		apple.onCollide("basket", () => {
+			score += apple.points;
+			scoreText.text = "Score: " + score;
+			apple.destroy();
+		});
+
+		apple.onCollide("ground", () => {
+			apple.destroy();
+		});
+
+		apple.onUpdate(() => {
+			if (apple.pos.y > 750) {
+				apple.destroy();
+			}
+		});
+	});
+
+	// Schnellere Steuerung
+	k.onKeyDown("d", () => {
+		if (basket.pos.x < 1250) {
+			basket.pos.x += 12;
+		}
+	});
+
+	k.onKeyDown("a", () => {
+		if (basket.pos.x > 0) {
+			basket.pos.x -= 12;
+		}
+	});
+
+	k.onKeyDown("right", () => {
+		if (basket.pos.x < 1250) {
+			basket.pos.x += 12;
+		}
+	});
+
+	k.onKeyDown("left", () => {
+		if (basket.pos.x > 0) {
+			basket.pos.x -= 12;
+		}
+	});
 });
 
-k.onKeyDown("right", () => {
-	if (basket.pos.x < 1250) {
-		basket.pos.x += 10;
-	}
-});
-
-k.onKeyDown("left", () => {
-	if (basket.pos.x > 0) {
-		basket.pos.x -= 10;
-	}
-});
+// Spiel starten
+k.go("level1");
 
 export default k;
