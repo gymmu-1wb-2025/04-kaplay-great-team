0ab272d3f2fd9c7468d02f532d20383bba8597a5
Sara Camenzind <e273618@sbl.ch>
2026-04-10 13:43:36 +0200

    korb schneller gemacht, korb fängt die äpfel jetzt

--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ const k = kaplay({
 	width: 1350,
 	height: 750,
 	canvas: document.getElementById("game-canvas"),
-	background: [135, 206, 235],
+	background: [135, 206, 235], // Himmelblau
 	global: false,
 	debug: true,
 	debugKey: "r",
 });
 
-k.setGravity(800);
+// Äpfel langsamer fallen lassen
+k.setGravity(350);
 
+// Score
 let score = 0;
 
 const scoreText = k.add([
@@ -20,23 +22,36 @@ const scoreText = k.add([
 	k.color(255, 255, 255),
 ]);
 
+// Boden
 k.add([
 	k.rect(1350, 80),
 	k.pos(0, 670),
 	k.color(34, 139, 34),
 	k.area(),
 	k.body({ isStatic: true }),
+	"ground",
 ]);
 
+// KORB
+// Wir machen den Korb aus einem Hauptobjekt mit Trefferfläche
 const basket = k.add([
-	k.rect(80, 40),
-	k.pos(620, 610),
+	k.rect(100, 40),
+	k.pos(620, 590),
 	k.color(139, 69, 19),
 	k.area(),
+	k.body({ isStatic: true }),
 	"basket",
 ]);
 
-k.loop(1.5, () => {
+// Damit es mehr wie ein Korb aussieht
+basket.add([k.rect(100, 10), k.pos(0, 30), k.color(101, 67, 33)]);
+
+basket.add([k.rect(10, 40), k.pos(0, 0), k.color(101, 67, 33)]);
+
+basket.add([k.rect(10, 40), k.pos(90, 0), k.color(101, 67, 33)]);
+
+// Äpfel spawnen
+k.loop(2, () => {
 	const isRed = k.rand(0, 1) > 0.5;
 	const appleColor = isRed ? [255, 0, 0] : [0, 200, 0];
 	const points = isRed ? 1 : -1;
@@ -48,14 +63,22 @@ k.loop(1.5, () => {
 		k.area(),
 		k.body(),
 		{ points: points },
+		"apple",
 	]);
 
+	// Wenn Apfel den Korb trifft
 	apple.onCollide("basket", () => {
 		score += apple.points;
 		scoreText.text = "Score: " + score;
 		apple.destroy();
 	});
 
+	// Wenn Apfel den Boden trifft
+	apple.onCollide("ground", () => {
+		apple.destroy();
+	});
+
+	// Falls ein Apfel ganz rausfällt
 	apple.onUpdate(() => {
 		if (apple.pos.y > 750) {
 			apple.destroy();
@@ -63,27 +86,28 @@ k.loop(1.5, () => {
 	});
 });
 
+// SCHNELLERE STEUERUNG
 k.onKeyDown("d", () => {
-	if (basket.pos.x < 1270) {
-		basket.move(300, 0);
+	if (basket.pos.x < 1250) {
+		basket.pos.x += 10;
 	}
 });
 
 k.onKeyDown("a", () => {
 	if (basket.pos.x > 0) {
-		basket.move(-300, 0);
+		basket.pos.x -= 10;
 	}
 });
 
 k.onKeyDown("right", () => {
-	if (basket.pos.x < 1270) {
-		basket.move(300, 0);
+	if (basket.pos.x < 1250) {
+		basket.pos.x += 10;
 	}
 });
 
 k.onKeyDown("left", () => {
 	if (basket.pos.x > 0) {
-		basket.move(-300, 0);
+		basket.pos.x -= 10;
 	}
 });
 
